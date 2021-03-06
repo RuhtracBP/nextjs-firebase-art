bd6667d19cad16e46329bed11e32e9e35f3eea5e
ArthurWSL <arthurhcbp@gmail.com>
2021-03-06 19:53:20 -0300

    doin auth in context to sing up

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,125 @@
+
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Avatar, TextField } from '@material-ui/core';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Testing Nxtjs and firebase
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+
+        <Avatar className={classes.avatar}>
+          <AccountCircleIcon />
+        </Avatar>
+        
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            
+            <Grid item xs={12}>
+              <TextField
+                variant="standard"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="standard"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="standard"
+                required
+                fullWidth
+                name="password"
+                label="Password Confirmation"
+                type="password"
+                id="password-confirm"
+                autoComplete="current-password"
+              />
+            </Grid>
+            
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase';
+
+interface AuthContextData {
+    currentUser: string;
+    signUp: ()=> string;
+}
+
+interface userData {
+    email: string;
+    password: string;
+    prevState: null;
+}
+
+const AuthContext = React.createContext({} as AuthContextData);
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({children}) {
+
+    const [currentUser, setCurrentUser] = useState();
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+        })
+        return unsubscribe;
+    }, [])
+
+    const value = {
+        currentUser,
+        signUp,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
